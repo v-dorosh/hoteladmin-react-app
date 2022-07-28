7391071bc10f111371dd14ffeec8c3f01da2be99
Vitaliy Dorosh <vitaliy.dorosh@edvantis.com>
2022-07-28 18:06:45 +0300

    added reportWebVitals file

--- src/components/FeaturedRooms.jsx
+++ src/components/FeaturedRooms.jsx
@@ -8,7 +8,7 @@ export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
 
   render() {
-    let { loading, featuredRooms: rooms } = this.context;
+    let { loading, featuredRooms:rooms } = this.context;
 
     rooms = rooms.map(room => {
       return <Room key={room.id} room={room} />;
